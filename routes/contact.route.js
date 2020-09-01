3585bb265a6e7de69d218c80d83b9d7d65c5038a
nhcminh303 <nhcminh303@gmail.com>
2020-09-02 00:26:11 +0700

    Update *

--- routes/contact.route.js
+++ routes/contact.route.js
@@ -1,38 +1,44 @@
 const nodemailer = require('nodemailer');
 const router = require('express').Router();
 
-router.get('/', (req, res) => res.render('contact'));
+router
+  .route('/')
+  .get((req, res) => {
+    res.render('contact', {
+      title: 'Contact - nodemailer'
+    });
+  })
+  .post((req, res) => {
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        // Email and password of the application
+        user: process.env.APP_EMAIL,
+        pass: process.env.APP_PASSWORD
+      }
+    });
 
-router.post('/', (req, res) => {
-	var transporter = nodemailer.createTransport({
-		service: 'gmail',
-		auth: {
-			user: process.env.EMAIL, // Email and password of the application
-			pass: process.env.PASSWORD
-		}
-	});
+    let mailOptions = {
+      from: '"nodejs-mailer project" <' + process.env.APP_EMAIL + '>',
+      to: req.body.email, // user's email
+      subject: 'Sending Email with `nodemailer`',
+      html: `
+        <h1>Details</h1>
+        <p>Your Name: ${req.body.name}</p>
+        <p>Your Role: ${req.body.role}</p>
+        <p>Your Company: ${req.body.company}</p>
+        <p>Your Email: ${req.body.email}</p>
+        <h3>Your Message</h3>
+        <p>${req.body.message}</p>
+      `
+    };
 
-	var mailOptions = {
-		from: '"Domain Name Of The Application" <' + process.env.EMAIL + '>',
-		to: process.env.RECEIVER_EMAIL,
-		subject: 'Contact (test nodemailer)',
-		html: `
-			<h3>Details</h3>
-			<p>Name: ${req.body.name}</p>
-			<p>Company: ${req.body.company}</p>
-			<p>Email: ${req.body.email}</p>
-			<p>Phone: ${req.body.phone}</p>
-			<h3>Message</h3>
-			<p>${req.body.message}</p>
-		`
-	};
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) return console.log(err);
+      else return console.log('Email sent: ' + info.response);
+    });
 
-	transporter.sendMail(mailOptions, (err, info) => {
-		if (err) return console.log(err);
-		else return console.log('Email sent: ' + info.response);
-	});
-
-	res.render('contact', { message: 'Email has been sent!' });
-});
+    res.render('contact', { msg: 'Email has been sent!' });
+  });
 
 module.exports = router;
